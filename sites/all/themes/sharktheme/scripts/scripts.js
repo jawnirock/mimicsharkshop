c3e99d56b7fb4aa702282d53f668a80bc49281f9
mmalmeida <mda.almeida@gmail.com>
2017-12-20 18:20:58 +0000

    Cleanup Scripts

--- sites/all/themes/sharktheme/scripts/scripts.js
+++ sites/all/themes/sharktheme/scripts/scripts.js
@@ -6,94 +6,6 @@ function animationOut(i){}
 function animationIn(i){}
 
 
-
-
-// function draw() {
-//     if (window.requestAnimationFrame) window.requestAnimationFrame(draw);
-//     // IE implementation
-//     else if (window.msRequestAnimationFrame) window.msRequestAnimationFrame(draw);
-//     // Firefox implementation
-//     else if (window.mozRequestAnimationFrame) window.mozRequestAnimationFrame(draw);
-//     // Chrome implementation
-//     else if (window.webkitRequestAnimationFrame) window.webkitRequestAnimationFrame(draw);
-//     // Other browsers that do not yet support feature
-//     else setTimeout(draw, 16.7);
-//     DrawVideoOnCanvas();
-// }
-
-// function DrawVideoOnCanvas() {
-//     var object = document.getElementById("videodata")
-//     var width = object.width;
-//     var height = object.height;
-//     var canvas = document.getElementById("videoscreen");
-//     canvas.setAttribute('width', width);
-//     canvas.setAttribute('height', height);
-//     if (canvas.getContext) {
-//         var context = canvas.getContext('2d');
-//         context.drawImage(object, 0, 0, width, height);
-//         var imgDataNormal = context.getImageData(0, 0, width, height);
-//         var imgData = context.createImageData(width, height);
-
-//         for (i = 0; i < imgData.width * imgData.height * 4; i += 4) {
-//             var r = imgDataNormal.data[i + 0];
-//             var g = imgDataNormal.data[i + 1];
-//             var b = imgDataNormal.data[i + 2];
-//             var a = imgDataNormal.data[i + 3];
-//             // set rgb levels for green and set alphachannel to 0;
-//             selectedR = 110;
-//             selectedG = 154;
-//             selectedB = 90;
-//             if (r <= selectedR && g >= selectedG && b >= selectedB) {
-//                 a = 0;
-//             }
-//             if (a != 0) {
-//                 imgData.data[i + 0] = r;
-//                 imgData.data[i + 1] = g;
-//                 imgData.data[i + 2] = b;
-//                 imgData.data[i + 3] = a;
-//             }
-//         }
-//         // For image anti-aliasing
-//         for (var y = 0; y < imgData.height; y++) {
-//             for (var x = 0; x < imgData.width; x++) {
-//                 var r = imgData.data[((imgData.width * y) + x) * 4];
-//                 var g = imgData.data[((imgData.width * y) + x) * 4 + 1];
-//                 var b = imgData.data[((imgData.width * y) + x) * 4 + 2];
-//                 var a = imgData.data[((imgData.width * y) + x) * 4 + 3];
-//                 if (imgData.data[((imgData.width * y) + x) * 4 + 3] != 0) {
-//                     offsetYup = y - 1;
-//                     offsetYdown = y + 1;
-//                     offsetXleft = x - 1;
-//                     offsetxRight = x + 1;
-//                     var change = false;
-//                     if (offsetYup > 0) {
-//                         if (imgData.data[((imgData.width * (y - 1)) + (x)) * 4 + 3] == 0) {
-//                             change = true;
-//                         }
-//                     }
-//                     if (offsetYdown < imgData.height) {
-//                         if (imgData.data[((imgData.width * (y + 1)) + (x)) * 4 + 3] == 0) {
-//                             change = true;
-//                         }
-//                     }
-//                     if (offsetXleft > -1) {
-//                         if (imgData.data[((imgData.width * y) + (x - 1)) * 4 + 3] == 0) {
-//                             change = true;
-//                         }
-//                     }
-//                     if (offsetxRight < imgData.width) {
-//                         if (imgData.data[((imgData.width * y) + (x + 1)) * 4 + 3] == 0) {
-//                             change = true;
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-//         context.putImageData(imgData, 0, 0);
-//     }
-// }
-
-
 var setVanuatuTime = function(){
 	d = new Date();
 	hours = d.getHours();
@@ -346,15 +258,10 @@ var siteMapCarouel = function() {
 
 $(document).ready(function() {
 
-  // $('body').kycoPreload();
 
 	mobileResponse();
 	siteMapCarouel();
 	
-	
-//	draw();
-
-
 	$("#block-commerce-cap-cap").addClass("firstStep");
 	checkoutTableFooter();
 	checkoutNumbers();
@@ -582,11 +489,7 @@ $(document).ready(function() {
 
 
 	setVanuatuTime();	
-
 	shopFunctions();
-
-	// hoverRipples();
-
 	
 });
 
@@ -606,13 +509,7 @@ $(window).resize(function() {
 })(jQuery);
 
 
-	
 
-// $('#bullets>li').on('click', function() {
-// 	openIndex($(this).index() + 1);
-// });
-
-	
 //for scroll by mouse or MAC track pad
 //   var indicator = new WheelIndicator({
 //   callback: function(e){   
